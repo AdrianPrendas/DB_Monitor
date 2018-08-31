0eab631a64f595b29ea991787dd2b2feb57cb31a
Adrian Prendas <a6r1an@hotmail.com>
2018-08-31 00:19:53 -0600

    sin datos quemados

--- DBStorageMonitor/web/js/loadIndex.js
+++ DBStorageMonitor/web/js/loadIndex.js
@@ -1,114 +1,115 @@
 var Controller = Controller || {}
-var avg = 0
+
 
 $(function () {
-    
+
 
     $("#btn-connect").on("click", function () {
         var intervalo = $("#time").val()
         var muestras = $("#samples").val()
-        if(isNaN(parseInt(muestras))){
-            return 
+        if (isNaN(parseInt(muestras))) {
+            return
         }
-        $("#btn-connect").prop("disabled",true);
-        $("#time").prop("disabled",true);
-        $("#samples").prop("disabled",true);
-        
+        $("#btn-connect").prop("disabled", true);
+        $("#time").prop("disabled", true);
+        $("#samples").prop("disabled", true);
+
         countDown(intervalo, muestras)
         consult()
     })
     $('#datetimepicker').datetimepicker({
         pickDate: false
     });
- 
+
 });
 
-function consult(){
+function consult() {
     Proxy.getTablespaces(function (res) {
-        
+
         //*** local - sotrage ***
-        var storage = {tuples:[],increases:[],table:[]}
-        var table = []
+        var storage = {} // {a1:{size:0,free:[],used:[]}}
         storage = Storage.retrieve("binnacle");
         if (storage == null) {
-            var storage = {tuples:[],increases:[],table:[]}
+            var storage = {} // [{size:0,free:[],used:[]}]
         }
-        
-        if(storage.tuples.length!=0){
-            var oldtuple = storage.tuples.pop()
-            var newtuple = res
-            
-            for(var i=0;i<newtuple.length;i++){
-                storage.increases.push({
-                    tablespace:newtuple[i][0],
-                    increase:newtuple[i][3]-oldtuple[i][3]
-                })
+        console.log(storage)
+
+        for (var i = 0; i < res.length; i++) {
+            if (storage[res[i][0]] == null) {
+                storage[res[i][0]] = {size: res[i][1], free: [res[i][2]], used: [res[i][3]], increase:[], avg:0, interval:""}
+            } else {
+                storage[res[i][0]].size = res[i][1]
+                storage[res[i][0]].free.push(res[i][2])
+                storage[res[i][0]].used.push(res[i][3])
+                storage[res[i][0]].interval = $("#time").val()
+                if(storage[res[i][0]].used.length>1){
+                    var arr = storage[res[i][0]].used
+                    var incremento = arr[arr.length-1]-arr[arr.length-2]
+                    storage[res[i][0]].increase.push(incremento)
+                    var sum = storage[res[i][0]].increase.reduce(function(a,b){return a+b})
+                    var avg = sum/storage[res[i][0]].increase.length
+                    storage[res[i][0]].avg = avg
+                }
             }
-            
-            
-            
-            
-            //res.puhs(85)//humbral
-            
-            Controller.createTable(res)
-            Controller.createBarChart(res)
-            Controller.createPieChart(res,0)   
         }
-        
-        storage.tuples.push(res)
+
+        Controller.createTable(res)
+        Controller.createBarChart(res)
+        Controller.createPieChart(res, 0)
+
         Storage.store("binnacle", storage);
         console.log("new record in local storage");
         console.log(storage)
         //*** local - sotrage ***               
     })
-    
+
 }
 
 function countDown(timePeriod, samples) {
-    
-    if(samples==0){
-        $("#btn-connect").prop("disabled",false);
-        $("#time").prop("disabled",false);
-        $("#samples").prop("disabled",false);
+
+    if (samples == 0) {
+        $("#btn-connect").prop("disabled", false);
+        $("#time").prop("disabled", false);
+        $("#samples").prop("disabled", false);
         document.getElementById("demo").innerHTML = "Finish";
         return
     }
-        
-      
-      var parts = timePeriod.split(/:/);
-var timePeriodMillis = (parseInt(parts[0], 10) * 60 * 60 * 1000) +
-                       (parseInt(parts[1], 10) * 60 * 1000) + 
-                       (parseInt(parts[2], 10) * 1000);
-        
-        
-        var countDownDate = new Date().getTime()+timePeriodMillis
+
+
+    var parts = timePeriod.split(/:/);
+    var timePeriodMillis = (parseInt(parts[0], 10) * 60 * 60 * 1000) +
+            (parseInt(parts[1], 10) * 60 * 1000) +
+            (parseInt(parts[2], 10) * 1000);
+
+
+    var countDownDate = new Date().getTime() + timePeriodMillis
 
 // Update the count down every 1 second
-        var x = setInterval(function () {
+    var x = setInterval(function () {
 
-            // Get todays date and time
-            var now = new Date().getTime();
+        // Get todays date and time
+        var now = new Date().getTime();
 
-            // Find the distance between now and the count down date
-            var distance = countDownDate - now;
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
 
-            // Time calculations for days, hours, minutes and seconds
-            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-            // Output the result in an element with id="demo"
-            document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-                    + minutes + "m " + seconds + "s ";
+        // Output the result in an element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+                + minutes + "m " + seconds + "s ";
 
-            // If the count down is over, write some text 
-            if (distance < 0) {
-                clearInterval(x);
-                document.getElementById("demo").innerHTML = "Consult..."
-                consult()
-                $("#samples").val(samples-1)
-                countDown(timePeriod, samples-1)
-            }
-        }, 1000);
-    }
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "Consult..."
+            consult()
+            $("#samples").val(samples - 1)
+            countDown(timePeriod, samples - 1)
+        }
+    }, 1000);
+}
