7b918e2055b734f511dad36d967ae48a16763d39
Addiel <addielo.oamvi@gmail.com>
2018-10-09 01:02:39 -0600

    ignores

--- DBMonitor/src/java/cr/ac/una/bases2/dbstoragemonitor/dao/ABaseDAO.java
+++ DBMonitor/src/java/cr/ac/una/bases2/dbstoragemonitor/dao/ABaseDAO.java
@@ -1,6 +1,5 @@
 package cr.ac.una.bases2.dbstoragemonitor.dao;
 
-import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -20,7 +19,7 @@ abstract class ABaseDAO {
     protected void conectar() throws SQLException,ClassNotFoundException 
     {   
         Class.forName("oracle.jdbc.driver.OracleDriver");
-        conexion = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/Quiz_1","sys as sysdba","manager");
+        conexion = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/DBLG2","sys as sysdba","k1n9r4d2");
 
     }
     
